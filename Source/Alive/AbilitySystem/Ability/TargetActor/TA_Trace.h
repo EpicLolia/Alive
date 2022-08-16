1219ee827320eceb5609ef7cba33d13cc3389b2a
EpicLolia <626009052@qq.com>
2022-08-16 19:08:56 +0800

    创建TargetActor和蓝图AsyncTask

---
+++ Source/Alive/AbilitySystem/Ability/TargetActor/TA_Trace.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbilityTargetActor.h"
+#include "TA_Trace.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALIVE_API ATA_Trace : public AGameplayAbilityTargetActor
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Alive/AbilitySystem/BlueprintAsyncTask/GameplayTagAddedRemoved.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "GameplayTagAddedRemoved.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALIVE_API UGameplayTagAddedRemoved : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+	
+};
