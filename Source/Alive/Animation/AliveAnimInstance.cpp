cfb337332c6f96c0659c1ecb28df8e105207e6e0
EpicLolia <626009052@qq.com>
2022-08-19 10:41:31 +0800

    准备编写站蹲趴姿势切换，在AnimInstance中添加GameplayTagPropertyMap用于更新tag

--- Source/Alive/Animation/AliveAnimInstance.cpp
+++ Source/Alive/Animation/AliveAnimInstance.cpp
@@ -3,3 +3,32 @@
 
 #include "Animation/AliveAnimInstance.h"
 
+#include "AbilitySystemGlobals.h"
+
+UAliveAnimInstance::UAliveAnimInstance()
+{
+}
+
+void UAliveAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if(!bHasInitializeWithAbilitySystem)
+	{
+		if (AActor* OwningActor = GetOwningActor())
+		{
+			if (UAbilitySystemComponent* ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwningActor))
+			{
+				InitializeWithAbilitySystem(ASC);
+				bHasInitializeWithAbilitySystem = true;
+			}
+		}
+	}
+}
+
+void UAliveAnimInstance::InitializeWithAbilitySystem(UAbilitySystemComponent* ASC)
+{
+	check(ASC);
+
+	GameplayTagPropertyMap.Initialize(this, ASC);
+}
--- Source/Alive/Animation/AliveAnimInstance.h
+++ Source/Alive/Animation/AliveAnimInstance.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
 #include "Animation/AnimInstance.h"
 #include "AliveAnimInstance.generated.h"
 
@@ -13,5 +14,20 @@ UCLASS()
 class ALIVE_API UAliveAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	UAliveAnimInstance();
 	
+protected:
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+	void InitializeWithAbilitySystem(UAbilitySystemComponent* ASC);
+	
+	// Gameplay tags that can be mapped to blueprint variables. The variables will automatically update as the tags are added or removed.
+	// These should be used instead of manually querying for the gameplay tags.
+	UPROPERTY(EditDefaultsOnly, Category = "GameplayTags")
+	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
+
+private:
+	bool bHasInitializeWithAbilitySystem = false;
 };
