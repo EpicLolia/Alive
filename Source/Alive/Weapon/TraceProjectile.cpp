9cd73d83e539fee7a66ba792fe4dc63325379135
EpicLolia <626009052@qq.com>
2022-08-26 16:41:24 +0800

    继续完善子弹traceProjectile

--- Source/Alive/Weapon/TraceProjectile.cpp
+++ Source/Alive/Weapon/TraceProjectile.cpp
@@ -8,33 +8,37 @@ DEFINE_LOG_CATEGORY_STATIC(LogTraceProjectile, Log, All);
 
 // Sets default values
 ATraceProjectile::ATraceProjectile()
-	: BulletType(EBulletType::Hitscan)
+	: ClipAmmo(30)
+	  , BulletType(EBulletType::ProjectileAndGravity)
 	  , UpdateFrequency(10)
-	  , Range(120000.0f)
-	  , Velocity(60000.0f)
-	  , Gravity(980.0f)
-	  , bDrawDebug(false)
-	  , ElapsedTimeSinceLastUpdate(0.0f)
-	  , UpdateInterval(0.1f)
-	  , ProjectileLifespan(1.0f)
+	  , bDrawDebug(true)
+	  , Range(1200.0f)
+	  , Velocity(600.0f)
+	  , Gravity(9.8f)
 {
+	ElapsedTimeSinceLastUpdate = 0.0f;
+	UpdateInterval = 0.1f;
+	ProjectileLifespan = 1.0f;
 	PrimaryActorTick.bCanEverTick = true;
-	UE_LOG(LogTraceProjectile, Warning, TEXT("[TraceProjectile]: Range is: %f"), Range);
 }
 
-void ATraceProjectile::Fire(FVector Location, FRotator Rotation, int32 BulletNum)
+void ATraceProjectile::FireOnce(FVector Location, FRotator Rotation, int32 ProjectileID)
 {
-	while ((BulletNum--) > 0)
+	if (ProjectileID > ProjectileInstances.Num() || ProjectileID < 1)
 	{
-		ProjectileInstances.Add(FProjectileInstanceInfo(Location, Rotation));
+		UE_LOG(LogTraceProjectile, Warning, TEXT("[TraceProjectile] FireOnce: ProjectileID Error!"));
+		return;
 	}
+	ProjectileInstances[ProjectileID - 1].Activate(Location, Rotation);
 }
 
 void ATraceProjectile::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTraceProjectile, Warning, TEXT("[TraceProjectile]: Range is: %f"), Range);
+	ClipAmmo = FMath::Clamp(ClipAmmo, 1, 999);
+	ProjectileInstances.SetNum(ClipAmmo);
+	ProjectileInstances.Shrink();
 
 	UpdateInterval = 1.0f / static_cast<float>(UpdateFrequency);
 	ProjectileLifespan = Range / Velocity;
@@ -47,37 +51,19 @@ void ATraceProjectile::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	ElapsedTimeSinceLastUpdate += DeltaTime;
-
-	TArray<FProjectileInstanceInfo> InstancesToRemove;
+	// Only loop once in theory
 	while (ElapsedTimeSinceLastUpdate >= UpdateInterval)
 	{
 		ElapsedTimeSinceLastUpdate -= UpdateInterval;
+
 		for (auto& Projectile : ProjectileInstances)
 		{
-			Projectile.ElapsedTime += UpdateInterval;
-			if (Projectile.ElapsedTime >= ProjectileLifespan)
+			if (Projectile.bIsActivate)
 			{
-				Projectile.ElapsedTime = ProjectileLifespan;
-				InstancesToRemove.Add(Projectile);
-			}
-			FVector PreviousLocation = Projectile.LocationWithoutGravity;
-			Projectile.LocationWithoutGravity = Projectile.Rotation.Vector() * Velocity * Projectile.ElapsedTime;
-			Projectile.GravityOffset = 0.5f * Gravity * FMath::Square(Projectile.ElapsedTime);
-
-			if (BulletType == EBulletType::Projectile)
-			{
-				TraceAndDrawDebug(PreviousLocation, Projectile.LocationWithoutGravity);
-			}
-			else if (BulletType == EBulletType::ProjectileAndGravity)
-			{
-				TraceAndDrawDebug(PreviousLocation, Projectile.LocationWithoutGravity - Projectile.GravityOffset);
+				UpdateProjectileOneStep(Projectile, GetAppropriateTimeStep(Projectile, UpdateInterval));
+				CheckProjectileResult(Projectile);
 			}
 		}
-	}
-
-	for (const auto& Instance : InstancesToRemove)
-	{
-		ProjectileInstances.RemoveSingleSwap(Instance);
 	}
 }
 
@@ -85,7 +71,63 @@ void ATraceProjectile::ProjectileHitEvent_Implementation(const FHitResult HitRes
 {
 }
 
-void ATraceProjectile::TraceAndDrawDebug(const FVector Start, const FVector End) const
+void ATraceProjectile::UpdateProjectileWithTime(FProjectileInstanceInfo& Projectile, float ElapsedTime)
+{
+	while (ElapsedTime > 0.0f)
+	{
+		UpdateProjectileOneStep(Projectile, GetAppropriateTimeStep(Projectile, ElapsedTime));
+		ElapsedTime -= UpdateInterval;
+	}
+}
+
+float ATraceProjectile::GetAppropriateTimeStep(const FProjectileInstanceInfo& Projectile, float ElapsedTime) const
+{
+	return FMath::Clamp(FMath::Min(ProjectileLifespan - Projectile.ElapsedTime, ElapsedTime), 0.0f, UpdateInterval);
+}
+
+void ATraceProjectile::UpdateProjectileOneStep(FProjectileInstanceInfo& Projectile, float TimeStep)
+{
+	const FVector PreLocationWithoutGravity = Projectile.LocationWithoutGravity;
+	const FVector PreLocationWithGravity = Projectile.LocationWithoutGravity - FVector(
+		0.0f, 0.0f, Projectile.GravityOffset);
+
+	Projectile.ElapsedTime += TimeStep;
+	Projectile.LocationWithoutGravity += Projectile.Rotation.Vector() * 100.0f * Velocity * TimeStep;
+	Projectile.GravityOffset = 0.5f * 100.0f * Gravity * FMath::Square(Projectile.ElapsedTime);
+
+	TArray<FHitResult> HitResults;
+	if (BulletType == EBulletType::Projectile)
+	{
+		TraceAndDrawDebug(HitResults, PreLocationWithoutGravity, Projectile.LocationWithoutGravity);
+	}
+	else if (BulletType == EBulletType::ProjectileAndGravity)
+	{
+		TraceAndDrawDebug(HitResults, PreLocationWithGravity,
+		                  Projectile.LocationWithoutGravity - FVector(
+			                  0.0f, 0.0f, Projectile.GravityOffset));
+	}
+	Projectile.HitResults.Append(HitResults);
+}
+
+void ATraceProjectile::CheckProjectileResult(FProjectileInstanceInfo& Projectile)
+{
+	// Projectile hit something
+	for (const auto& HitResult : Projectile.HitResults)
+	{
+		OnProjectileHit.Broadcast(HitResult);
+		Projectile.bIsActivate = false;
+		UE_LOG(LogTraceProjectile, Warning, TEXT("[TraceProjectile] CheckProjectileResult: Hit!"))
+
+	}
+
+	// Projectile time out
+	if (Projectile.ElapsedTime >= ProjectileLifespan - KINDA_SMALL_NUMBER)
+	{
+		Projectile.bIsActivate = false;
+	}
+}
+
+void ATraceProjectile::TraceAndDrawDebug(TArray<FHitResult>& HitResults, const FVector Start, const FVector End) const
 {
 	TArray<AActor*> ActorsToIgnore;
 	ActorsToIgnore.Add(GetInstigator());
@@ -94,12 +136,12 @@ void ATraceProjectile::TraceAndDrawDebug(const FVector Start, const FVector End)
 	Params.bReturnPhysicalMaterial = true;
 	Params.AddIgnoredActors(ActorsToIgnore);
 
-	TArray<FHitResult> HitResults;
+	// Only the overlap hit results and the first block hit result will be generated.
 	GetWorld()->LineTraceMultiByChannel(HitResults, Start, End,
 	                                    ECollisionChannel::ECC_Visibility, Params,
 	                                    FCollisionResponseParams());
 	if (bDrawDebug)
 	{
-		DrawDebugLine(GetWorld(), Start, End, FColor::Yellow, true, 3.0f);
+		DrawDebugLine(GetWorld(), Start, End, FColor::Yellow, false, 3.0f, 0, 5);
 	}
 }
--- Source/Alive/Weapon/TraceProjectile.h
+++ Source/Alive/Weapon/TraceProjectile.h
@@ -12,31 +12,40 @@ USTRUCT()
 struct ALIVE_API FProjectileInstanceInfo
 {
 	GENERATED_BODY()
-
-	FProjectileInstanceInfo(): LocationWithoutGravity(FVector(0.0f, 0.0f, 0.0f))
-	                           , Rotation(FRotator(0.0f, 0.0f, 0.0f))
-	                           , GravityOffset(0.0f), ElapsedTime(0.0f)
-	{
-	}
-
-	FProjectileInstanceInfo(FVector InLocation, FRotator InRotator)
-		: LocationWithoutGravity(InLocation), Rotation(InRotator)
-		  , GravityOffset(0.0f), ElapsedTime(0.0f)
-	{
-	}
-
+public:
 	FVector LocationWithoutGravity;
 	FRotator Rotation;
 
+	TArray<FHitResult> HitResults;
+
 	float GravityOffset;
 	float ElapsedTime;
+
+	bool bIsActivate;
+
+public:
+	FProjectileInstanceInfo(): LocationWithoutGravity(FVector(0.0f, 0.0f, 0.0f))
+	                           , Rotation(FRotator(0.0f, 0.0f, 0.0f))
+	                           , GravityOffset(0.0f), ElapsedTime(0.0f), bIsActivate(false)
+	{
+	}
 
 	bool operator ==(const FProjectileInstanceInfo& rhs) const
 	{
 		return this->Rotation.Equals(rhs.Rotation) &&
 			this->LocationWithoutGravity.Equals(rhs.LocationWithoutGravity) &&
-				FMath::Abs(this->GravityOffset-rhs.GravityOffset) <= KINDA_SMALL_NUMBER&&
-				FMath::Abs(this->ElapsedTime-rhs.ElapsedTime) <= KINDA_SMALL_NUMBER;
+			FMath::Abs(this->GravityOffset - rhs.GravityOffset) <= KINDA_SMALL_NUMBER &&
+			FMath::Abs(this->ElapsedTime - rhs.ElapsedTime) <= KINDA_SMALL_NUMBER;
+	}
+
+	void Activate(FVector InLocation, FRotator InRotator)
+	{
+		LocationWithoutGravity = InLocation;
+		Rotation = InRotator;
+		HitResults.Reset();
+		GravityOffset = 0.0f;
+		ElapsedTime = 0.0f;
+		bIsActivate = true;
 	}
 };
 
@@ -60,8 +69,13 @@ class ALIVE_API ATraceProjectile : public AActor
 public:
 	ATraceProjectile();
 
+	/**
+	 * Register a bullet for ticking
+	 *
+	 * @param ProjectileID Which projectile did you fire in the clip
+	 */
 	UFUNCTION(BlueprintCallable)
-	void Fire(FVector Location, FRotator Rotation, int32 BulletNum = 1);
+	void FireOnce(FVector Location, FRotator Rotation, int32 ProjectileID);
 
 	UPROPERTY(BlueprintAssignable)
 	FProjectileHitDelegate OnProjectileHit;
@@ -69,30 +83,32 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	/** This determines the maximum number of bullets that can be updated at the same time */
 	UPROPERTY(BlueprintReadOnly, Meta = (ExposeOnSpawn = true))
+	int32 ClipAmmo;
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Alive")
 	EBulletType BulletType;
-	/** MaxFrequency is decided by tick rate. (/s) */
-	UPROPERTY(BlueprintReadOnly, Meta = (ExposeOnSpawn = true, ClampMin = 1, ClampMax = 60))
+	/** MaxFrequency is decided by tick rate. */
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Alive", meta = (ClampMin = 1, ClampMax = 60))
 	int32 UpdateFrequency;
-	/** How far the bullet can go. (cm)*/
-	UPROPERTY(BlueprintReadOnly, Meta = (ExposeOnSpawn = true, ClampMin = 200.0f, ClampMax = 999999.9f))
-	float Range;
-	/** Will not work while the BulletType is hitscan. The Speed of the bullet. (cm/s)*/
-	UPROPERTY(BlueprintReadOnly, Meta = (ExposeOnSpawn = true, ClampMin = 200.0f, ClampMax = 999999.9f))
-	float Velocity;
-	/** Only work while BulletType is ProjectileAndGravity. (cm/s^2) */
-	UPROPERTY(BlueprintReadOnly, Meta = (ExposeOnSpawn = true, ClampMin = 100.0f, ClampMax = 999999.9f))
-	float Gravity;
-	/** Only work while BulletType is ProjectileAndGravity. */
-	UPROPERTY(BlueprintReadOnly, Meta = (ExposeOnSpawn = true))
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Alive")
 	bool bDrawDebug;
-
+	/** How far the bullet can go. (m) */
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Alive|Projectile", meta = (ClampMin = 1.0f, ClampMax = 3000.0f))
+	float Range;
+	/** Will not work while the BulletType is hitscan. The Speed of the bullet. (m/s) */
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Alive|Projectile", meta = (ClampMin = 1.0f, ClampMax = 1200.0f))
+	float Velocity;
+	/** Only work while BulletType is ProjectileAndGravity. (m/s^2) */
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Alive|Projectile", meta = (ClampMin = 0.1f, ClampMax = 100.0f))
+	float Gravity;
+	
 	virtual void Tick(float DeltaTime) override;
 
 	UFUNCTION(BlueprintNativeEvent)
 	void ProjectileHitEvent(const FHitResult HitResult);
 	virtual void ProjectileHitEvent_Implementation(const FHitResult HitResult);
-	
+
 private:
 	TArray<FProjectileInstanceInfo> ProjectileInstances;
 
@@ -102,5 +118,12 @@ private:
 	// Calculated by Range and velocity
 	float ProjectileLifespan;
 
-	void TraceAndDrawDebug(const FVector Start, const FVector End) const;
+	void TraceAndDrawDebug(TArray<FHitResult>& HitResults, const FVector Start, const FVector End) const;
+
+	// Take UpdateInterval, ElapsedTime and ProjectileLifspan in consideration.
+	float GetAppropriateTimeStep(const FProjectileInstanceInfo& Projectile, float ElapsedTime)const;
+	void UpdateProjectileWithTime(FProjectileInstanceInfo& Projectile, float ElapsedTime);
+	void UpdateProjectileOneStep(FProjectileInstanceInfo& Projectile, float TimeStep);
+	void CheckProjectileResult(FProjectileInstanceInfo& Projectile);
 };
+
